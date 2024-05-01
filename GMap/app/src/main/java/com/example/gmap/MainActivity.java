9c45a62f525cc6ffa77f87cc9767e6f0aca8eeee
manali0501 <manali.khedekar@walchandsangli.ac.in>
2024-05-01 17:42:19 +0530

    5th commit

---
+++ GMap/app/src/main/java/com/example/gmap/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.gmap;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Marker mMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in a specific location and move the camera
+        LatLng location = new LatLng(16.845820831505215, 74.60145749453896); // New York City coordinates
+        mMarker = mMap.addMarker(new MarkerOptions().position(location).title("Marker on WCE"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12)); // Zoom level 12
+    }
+}
