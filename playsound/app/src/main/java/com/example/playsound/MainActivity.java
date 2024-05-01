b32cf962152aa85c1df44315cf0e38a3bbd070bc
manali0501 <manali.khedekar@walchandsangli.ac.in>
2024-05-01 22:37:54 +0530

    16th commit

---
+++ playsound/app/src/main/java/com/example/playsound/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.playsound;
+
+
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound); // Load sound file
+        playButton = findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause(); // Pause if already playing
+                    } else {
+                        mediaPlayer.start(); // Start playing
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release(); // Release the media player when activity is destroyed
+        }
+    }
+}
